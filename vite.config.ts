35978f417a33f23eff9aa3f3fc3a5b39381f69cf
Joe-868 <liujieicm@gmail.com>
2026-06-09 08:53:10 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/crystallisation-explorer/",
-
   plugins: [react()],
-
   server: {
     port: 5173,
     host: true
